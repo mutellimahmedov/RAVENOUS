a32772117711e42362a6fa0e6735f48dc0b120cc
Mutellim <57439321+mutellimahmedov@users.noreply.github.com>
2020-08-02 18:02:20 +0300

    Add files via upload

---
+++ src/util/Yelp.js
@@ -0,0 +1,31 @@
+const apiKey = 'Ck19q-_6EZdIYUQvTQXBDx2-GnZ1OZmQvfZZVPxdm4XgR0VZSnmhE3MDMLQYNRFOFmRTb9Q9p0b7eHhmWF0b8lbOHwztdexJOR2qA9YHttpye2vSXNSmNSybKtYdX3Yx';
+
+const Yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zipCode,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    };
+                }));
+            }
+        })
+    }
+}
+export default Yelp;
